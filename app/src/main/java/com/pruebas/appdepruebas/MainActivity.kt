1a8327ecb29dfba10bcb9b7fe62520b0cf6256cd
GordonShumwey <r.serrano.rieloff@gmail.com>
2022-05-15 21:43:47 -0400

    segundo commit

--- app/src/main/java/com/pruebas/appdepruebas/MainActivity.kt
+++ app/src/main/java/com/pruebas/appdepruebas/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.pruebas.appdepruebas
 
 import androidx.appcompat.app.AppCompatActivity
@@ -7,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setup()
+    }
+
+    private fun setup(){
+        title = "prueba"
     }
 }
